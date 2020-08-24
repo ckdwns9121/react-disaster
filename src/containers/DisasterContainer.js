28a56e0a85c6bee672c7c1bb78252e55e3a99374
ckdwns9121 <ckdwns9121@naver.com>
2020-08-24 17:22:16 +0900

    인피니티 스크롤링

--- src/containers/DisasterContainer.js
+++ src/containers/DisasterContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { get_message } from '../store/disaster';
 import {getDisasterMsg} from '../api/getDisasterMsg';
@@ -10,37 +10,49 @@ import styles from './Disaster.module.scss';
 const DisasterContainer = () => {
 
     const { loading, data, pageNo, error } = useSelector(state => state.disaster);
-    const [value,setValue] = useState('');
+    const [search,setSearch] = useState('');
     const dispatch = useDispatch();
+
+    const getDisasterMessage = useCallback(async () => {
+        const responce = await getDisasterMsg(pageNo);
+        dispatch(get_message(responce));
+        console.log(responce);
+    },[dispatch]);
+
+    useEffect(()=>{
+    window.addEventListener('scroll',onScorll,true);
+    },[])
 
     useEffect(() => {
          getDisasterMessage();
-    },[]);
-    useEffect(()=>{
-        // console.log("데이터 받아옴");
-    },[data,value])
-    const onChageValue =(e)=>{
-        setValue(e.target.value);
+    },[getDisasterMessage]);
+
+
+    const onScorll =()=>{
+        let scrollHeight = Math.max(document.documentElement.scrollHeight,document.body.scrollHeight);
+        let scrollTop = Math.max (document.documentElement.scrollTop,document.body.scrollTop);
+        let clientHeight = document.documentElement.clientHeight;
+        if(scrollTop + clientHeight=== scrollHeight){
+            console.log("페이지 끝");
+            getDisasterMessage();
+        }
     }
 
-    const getDisasterMessage = async () => {
-        const responce = await getDisasterMsg(pageNo);
-        // console.log(responce);
-        dispatch(get_message(responce));
-        // console.log(pageNo);
-    }
+    const onChageSearch =useCallback((e)=>{
+        setSearch(e.target.value);
+    },[setSearch]);
 
     const render=()=>{
         const list = data.map((item,index) =>{
             return item.filter(v =>{
                 return (
-                    v.location_name.indexOf(value) !==-1
+                    v.location_name.indexOf(search) !==-1
                 )
             })
         })
         return (
             <>
-            <input className={styles['input']} type="text" placeholder="지역을 검색하세요" value={value} onChange={onChageValue} ></input>
+            <input className={styles['input']} type="text" placeholder="지역을 검색하세요" value={search} onChange={onChageSearch} ></input>
             <DisasterList value={list} pageNo={pageNo}/>
             <div className={styles['btn']} onClick ={getDisasterMessage}>
                 더보기
@@ -53,7 +65,6 @@ const DisasterContainer = () => {
     return (
         <div className={styles['list']}>
             {data.length===0 ? "로딩중" :render()}
-  
         </div>
         )
 }
