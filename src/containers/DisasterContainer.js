0b503f05a1a914eea1041525eb8529d97744cce7
박창준 <ckdwns9121@naver.com>
2020-09-29 00:10:52 +0900

    # 사가로 업데이트 해야함 무한스크롤링 추가

--- src/containers/DisasterContainer.js
+++ src/containers/DisasterContainer.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState,useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { get_message } from '../store/disaster';
-import {getDisasterMsg} from '../api/getDisasterMsg';
+import { get_message, get_message_success, get_message_error } from '../store/disaster';
+import { getDisasterMsg } from '../api/getDisasterMsg';
 import DisasterList from '../components/DisasterList';
 import styles from './Disaster.module.scss';
 
@@ -9,63 +9,87 @@ import styles from './Disaster.module.scss';
 
 const DisasterContainer = () => {
 
-    const { loading, data, pageNo, error } = useSelector(state => state.disaster);
-    const [search,setSearch] = useState('');
+    const { loading, data, error } = useSelector(state => state.disaster);
+    let page_number = useRef(1);
+
+    const [search, setSearch] = useState('');
     const dispatch = useDispatch();
 
-    const getDisasterMessage = useCallback(async () => {
-        const responce = await getDisasterMsg(pageNo);
-        dispatch(get_message(responce));
-        console.log(responce);
-    },[dispatch]);
+    const getDisasterMessage = async () => {
+        dispatch(get_message());
+        try {
+            const responce = await getDisasterMsg(page_number.current);
+            page_number.current += 1;
+            console.log(page_number.current);
+            console.log(responce);
+            dispatch(get_message_success(responce));
 
-    useEffect(()=>{
-    window.addEventListener('scroll',onScorll,true);
-    },[])
+        }
+        catch (e) {
+            console.error(e);
+            dispatch(get_message_error());
+        }
+
+    };
 
     useEffect(() => {
-         getDisasterMessage();
-    },[getDisasterMessage]);
+        window.addEventListener('scroll', onScorll, true);
+    }, [])
+
+    useEffect(() => {
+        getDisasterMessage();
+    }, []);
+    useEffect(() => {
+        console.log(page_number.current)
+        console.log('바뀜');
+    }, [page_number])
 
 
-    const onScorll =()=>{
-        let scrollHeight = Math.max(document.documentElement.scrollHeight,document.body.scrollHeight);
-        let scrollTop = Math.max (document.documentElement.scrollTop,document.body.scrollTop);
+    const onScorll = useCallback(() => {
+        let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+        let scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
         let clientHeight = document.documentElement.clientHeight;
-        if(scrollTop + clientHeight=== scrollHeight){
+        // console.log(scrollHeight);
+        // console.log(scrollTop + clientHeight);
+        let height = Math.round(scrollTop + clientHeight);
+        if (height === scrollHeight) {
             console.log("페이지 끝");
             getDisasterMessage();
         }
-    }
+    }, [getDisasterMessage]);
 
-    const onChageSearch =useCallback((e)=>{
+    const onChageSearch = useCallback((e) => {
         setSearch(e.target.value);
-    },[setSearch]);
+    }, [setSearch]);
 
-    const render=()=>{
-        const list = data.map((item,index) =>{
-            return item.filter(v =>{
+    const render = () => {
+        const list = data.map((item, index) => {
+            return item.filter(v => {
                 return (
-                    v.location_name.indexOf(search) !==-1
+                    v.location_name.indexOf(search) !== -1
                 )
             })
         })
         return (
             <>
-            <input className={styles['input']} type="text" placeholder="지역을 검색하세요" value={search} onChange={onChageSearch} ></input>
-            <DisasterList value={list} pageNo={pageNo}/>
-            <div className={styles['btn']} onClick ={getDisasterMessage}>
-                더보기
-            </div>
+                <input className={styles['input']} type="text" placeholder="지역을 검색하세요" value={search} onChange={onChageSearch} ></input>
+                <DisasterList value={list} />
+                {loading ? 'LOADING...' :
+
+                    <div className={styles['btn']} onClick={getDisasterMessage}>
+                        더보기
+                     </div>
+                }
+
             </>
         )
     }
 
-    
+
     return (
         <div className={styles['list']}>
-            {data.length===0 ? "로딩중" :render()}
+            {data.length === 0 ? "LOADING..." : render()}
         </div>
-        )
+    )
 }
 export default DisasterContainer;
--- src/store/disaster.js
+++ src/store/disaster.js
@@ -1,27 +1,47 @@
 import produce from 'immer';
 const GET_MESSAGE = "GET_MESSAGE";
+const GET_MESSAGE_SUCCESS = 'GET_MESSAGE_SUCCESS';
+const GET_MESSAGE_ERROR = 'GET_MESSAGE_ERROR';
 
-export const get_message = (data) => ({
-    type: GET_MESSAGE, data
+export const get_message = () => ({
+    type: GET_MESSAGE
 })
 
+export const get_message_success = (data) => ({
+    type: GET_MESSAGE_SUCCESS, data
+})
+export const get_message_error = () => ({
+    type: GET_MESSAGE_ERROR
+})
 const initState = {
     loading: false,
     data: [],
     error: false,
-    pageNo: 1,
 }
 
 export default function disaster(state = initState, action) {
     switch (action.type) {
         case GET_MESSAGE:
+            return produce(
+                state, draft => {
+                    draft.loading = true;
+                    draft.error = false;
+                }
+            )
+        case GET_MESSAGE_SUCCESS:
             const pushData = action.data;
             return produce(
-                state,draft =>{
+                state, draft => {
                     draft.data.push(pushData);
-                    draft.pageNo = draft.pageNo+1;
                     draft.loading = false;
                     draft.error = false;
+                }
+            )
+        case GET_MESSAGE_ERROR:
+            return produce(
+                state, draft => {
+                    draft.loading = false;
+                    draft.error = true;
                 }
             )
         default:
